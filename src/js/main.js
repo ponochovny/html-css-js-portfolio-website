858597052671ceb31e4b3f190b578bf957657ef1
ponochovny <ponochovny@gmail.com>
2024-05-12 18:36:29 +0300

    feat: slider, adaptive, animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,47 @@
+AOS.init()
+
 var swiper = new Swiper('.mySwiper', {
-	// Опции Swiper
-	slidesPerView: 1,
-	spaceBetween: 30,
+	slidesPerView: 'auto',
 	pagination: {
 		el: '.swiper-pagination',
 	},
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
+	// effect: 'coverflow',
+	grabCursor: true,
+	centeredSlides: true,
+	spaceBetween: 16,
+	initialSlide: 1,
+	breakpoints: {
+		// when window width is >= 320px
+		768: {
+			spaceBetween: 30,
+		},
 	},
+	// loop: true,
+	// coverflowEffect: {
+	// 	rotate: 0,
+	// 	stretch: 0,
+	// 	depth: 100,
+	// 	modifier: 1,
+	// 	scale: 0.7,
+	// 	slideShadows: true,
+	// },
+	// navigation: {
+	// 	nextEl: '.swiper-button-next',
+	// 	prevEl: '.swiper-button-prev',
+	// },
+	// on: {
+	// 	click() {
+	// 		console.log('index', this.clickedIndex)
+	// 		swiper2.slideTo(this.clickedIndex)
+	// 	},
+	// },
 })
+
+function toggleMenu() {
+	const menu = document.querySelector('.menu-links')
+	const icons = document.querySelectorAll('.hamburger-icon')
+	menu.classList.toggle('open')
+	icons.forEach((el) => {
+		el.classList.toggle('open')
+	})
+}
